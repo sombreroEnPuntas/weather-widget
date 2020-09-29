1f21fba75b7f6e9d95d8d23c2928b1da99d04ab3
Mati <matiasfornes@gmail.com>
2020-09-29 23:57:02 +0200

    fix(codeclimate): function Widget has 71 lines of code (allowance 50)

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react'
 import WeatherImage from '../WeatherImage'
+import Header from './Header'
 
 import useForecast from './useForecast'
 import styles from './Widget.module.css'
-
-const maxTemperatureReducer = (prev, { temperature }) =>
-  prev > temperature ? prev : temperature
-const minTemperatureReducer = (prev, { temperature }) =>
-  prev < temperature ? prev : temperature
 
 const Widget = () => {
   const { data, error } = useForecast()
@@ -17,52 +13,8 @@ const Widget = () => {
     <div className={styles.wrapper}>
       {data ? (
         <>
-          <header className={styles.header}>
-            <article className={styles.article}>
-              <WeatherImage
-                type={data.list[current].icon === '01d' ? '01d' : 'default'}
-              />
+          <Header current={current} data={data} />
 
-              <div>
-                <div className={styles.articleHeading}>
-                  {data.list[current].weather}
-                  {'  '}
-                  {data.list.reduce(
-                    maxTemperatureReducer,
-                    data.list[0].temperature
-                  )}
-                  ° /
-                  {data.list.reduce(
-                    minTemperatureReducer,
-                    data.list[0].temperature
-                  )}
-                  °
-                </div>
-                <div
-                  data-testid="articleOverlineBig"
-                  className={styles.articleOverlineBig}
-                >
-                  {data.list[current].temperature}°
-                </div>
-              </div>
-            </article>
-            <article className={styles.article}>
-              <div>
-                <div className={styles.articleHeading}>{data.cityName}</div>
-                <div className={styles.articleOverline}>
-                  <span style={{ whiteSpace: 'pre-line' }}>
-                    {new Date(data.list[current].timestamp * 1000)
-                      .toLocaleDateString('de-DE', {
-                        weekday: 'long',
-                        month: 'long',
-                        day: 'numeric',
-                      })
-                      .replace(',', '\n')}
-                  </span>
-                </div>
-              </div>
-            </article>
-          </header>
           <ul className={styles.hourlyData}>
             {data.list.map(({ date, icon, temperature, time }, index) => (
               <li className={styles.hourlyDataItem} key={`${date}-${time}`}>
